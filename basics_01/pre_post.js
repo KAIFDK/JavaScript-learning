1b48d2f57b40c217c998e5f8553c2588a7dfd0b4
KAIFDK <156346760+KAIFDK@users.noreply.github.com>
2025-11-11 08:18:58 +0000

    Add initial code examples and log gameCounter variable

---
+++ basics_01/pre_post.js
@@ -0,0 +1,10 @@
+// let a = 3
+// const b = ++a
+// console.log(`x : ${a} , y : ${b}`)
+
+// let X = 3
+// const Y = a++
+// console.log(`x : ${X} , y : ${Y}`)
+
+
+
--- basics_01/text.js
+++ basics_01/text.js
@@ -1,1 +1,6 @@
-console.log("Kaif")
+// console.log(2%3)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+var gameCounter;
+console.log(gameCounter);
