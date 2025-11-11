8f6c75ed24e1c466cf1d8b4eb74853a6ea222217
KAIFDK <156346760+KAIFDK@users.noreply.github.com>
2025-11-11 08:49:00 +0000

    check and strict check learning

---
+++ basics_01/conversion.js
@@ -0,0 +1,20 @@
+// console.log(2 > "1")
+
+// console.log(null == undefined) //value check conversion check
+// console.log(null === undefined) //type check strict check
+
+// console.log("5" === 5) //false Type check
+
+// console.log(null == 0) //false value check
+// console.log(null > 0) //false
+// console.log(null >= 0) //true
+
+// console.log(null == undefined) //True Equility conversion 
+
+// let stringNumber = "123"
+// const actualNumber = Number(stringNumber)
+// console.log(actualNumber == stringNumber)
+
+
+
+
